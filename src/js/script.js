20e56adc7bbc860a72f02a434561d8dec6528187
E1Bos <22bosluc@sssrome.it>
2022-06-05 17:38:11 +0200

    Added more pros and cons + a quote

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.pro-container').forEach(button => {
     button.addEventListener('click', () => {
-        button.getElementsByClassName("pro-content")[0].classList.toggle('pro-content--active');
         button.classList.toggle('pro-container--active');
+        button.getElementsByClassName("pro-content")[0].classList.toggle('pro-content--active');
         document.getElementsByClassName("all-pros")[0].classList.toggle('all-pros--hidden');
         document.body.classList.toggle('body--overflow');
     });
